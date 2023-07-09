b08574d07635a222e3d572a1e9dc235a0792910c
Juan Beltrame <juan_beltrame@hotmail.com>
2023-07-08 21:35:04 -0300

    Updates in C#

---
+++ C#/Ej5/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static int[] a = new int[10];
+    private static int[] b = new int[10];
+    private static int[] c = new int[10];
+    
+    Program (){
+        InicializarArreglo(a, 10);
+        InicializarArreglo(b, 10);
+        InicializarArreglo(c, 10);
+    }
+
+    private static void InicializarArreglo(int[] arreglo, int tamanoDelArray){
+        for (int i = 0; i < tamanoDelArray ; i++){
+            arreglo[i] = 0;
+        }
+    }
+
+    private static void CargarArreglo(int[] arreglo, int tamanoDelArray){
+        for (int i = 0; i < tamanoDelArray ; i++){
+            Console.WriteLine("Ingrese el valor para la posicion {0}", i);
+            arreglo[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        CargarArreglo(a, 10);
+    }
+}
---
+++ Python/One-Dimensional Array_Ej5.py
@@ -0,0 +1,10 @@
+# Practica 3 - One-Dimensional Array- Ejercicio 05
+
+def inicializarArray(arr, tam):
+    for i in range (0, tam):
+        arr[i] = 0
+
+
+a = [0] * 10
+
+inicializarArray(a, 10)
