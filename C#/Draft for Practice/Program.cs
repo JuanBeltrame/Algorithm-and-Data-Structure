7b2098e8bed578faf41fefa678c251fc18f2f45d
Juan Beltrame <juan_beltrame@hotmail.com>
2023-07-10 23:18:37 -0300

    Updates in the Excercise

---
+++ C#/Draft for Practice/Program.cs
@@ -0,0 +1,8 @@
+﻿int resultado = Sumar(5, 10);
+
+Console.WriteLine(nameof(Sumar)); // Imprime "Sumar" en la consola
+
+static int Sumar(int a, int b)
+{
+    return a + b;
+}
--- C#/Ej5/Program.cs
+++ C#/Ej5/Program.cs
@@ -1,24 +1,24 @@
 ﻿internal class Program
 {
-    private static int[] a = new int[10];
-    private static int[] b = new int[10];
-    private static int[] c = new int[10];
-    private static int[] sumaArr = new int[10];
+    private static int[] a = new int[3];
+    private static int[] b = new int[3];
+    private static int[] c = new int[3];
+    private static int[] sumaArr = new int[3];
 
     private static void InicializarArreglo(int[] arreglo, int tamanoDelArray){
-        for (int i = 0; i <= tamanoDelArray ; i++){
+        for (int i = 0; i < tamanoDelArray ; i++){
             arreglo[i] = 0;
         }
     }
 
     private static void CargarArreglo(int[] arreglo, int tamanoDelArray){
-        for (int i = 0; i <= tamanoDelArray ; i++){
+        for (int i = 0; i < tamanoDelArray ; i++){
             Console.WriteLine("Ingrese el valor para la posicion {0}", i);
             arreglo[i] = int.Parse(Console.ReadLine());
         }
     }
     private static int[] SumarArreglos(int[] arreglo1, int[] arreglo2, int[] arreglo3, int tamanoDelArray){
-        for (int i = 0; i <= tamanoDelArray -1 ; i++){
+        for (int i = 0; i < tamanoDelArray -1 ; i++){
             arreglo3[i] = arreglo1[i] + arreglo2[i];
         }
 
@@ -32,15 +32,35 @@
         }
         Console.WriteLine(arreglo3);
     }
+
+    private static void MostrarUnSoloArreglo(int[] unicoArreglo, int tamanoDelArray){
+        for (int i = 0; i < tamanoDelArray ; i++){
+            Console.WriteLine($"Arreglo: {unicoArreglo[i]}");
+        }
+    }
     private static void Main(string[] args)
     {
-        InicializarArreglo(a, 9);
+        InicializarArreglo(a, 3);
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("INGRESE LOS VALORES DEL ARREGLO A");
-        CargarArreglo(a, 9);
+        Console.ResetColor();
+        CargarArreglo(a, 3);
+
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("INGRESE LOS VALORES DEL ARREGLO B");
-        CargarArreglo(b, 10);
-        CargarArreglo(c, 10);
-        sumaArr = SumarArreglos(a, b, c, 10);
-        MostrarArreglos(a, b, sumaArr, 10);
+        Console.ResetColor();
+        CargarArreglo(b, 3);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("SUMA DE LOS ARREGLOS");
+        Console.ResetColor();
+        sumaArr = SumarArreglos(a, b, c, 3);
+
+        Console.WriteLine("Arreglo A");
+        MostrarUnSoloArreglo(a, 3);
+        Console.WriteLine("Arreglo B");
+        MostrarUnSoloArreglo(b, 3);
+        Console.WriteLine("Arreglo C");
+        MostrarUnSoloArreglo(sumaArr, 3);
     }
 }
