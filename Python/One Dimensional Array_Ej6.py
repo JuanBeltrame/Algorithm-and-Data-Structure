ab87acc12de3a4862847061dbbff8f249fecfabf
Juan Beltrame <juan_beltrame@hotmail.com>
2023-07-14 00:07:11 -0300

    New Python File

---
+++ Python/One Dimensional Array_Ej6.py
@@ -0,0 +1,44 @@
+"""
+The grades for a midterm exam are available for the 30 students in a class.
+It is desired to know how many of them obtained a grade higher than the course average.
+"""
+
+
+def load_notes(arr, tam):
+    begin = 0
+    for i in range(begin, tam):
+        arr[i] = int(input("Upload Score: "))
+
+
+def get_average(arr, size):
+    total_score = 0
+    begin = 0
+
+    for i in range(begin, size):
+        total_score += arr[i]
+
+    average = total_score / size
+    return average
+
+
+def get_scores(arr, size, p):
+    begin = 0
+    c = 0
+    for i in range(begin, size):
+        if arr[i] > p:
+            c += 1
+
+        else:
+            print()
+
+    return c
+
+
+# Main Program
+T = 3
+array = [0] * T
+print("Upload notes from exam")
+load_notes(array, T)
+avg = get_average(array[:], T)
+print("The average is ", avg)
+print("The number of students above the score is.", get_scores(array, 3, avg))
